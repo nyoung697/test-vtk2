d06ca8ba4e78d48db1924a74564191d3041d2295
Nick Young <nyoung697@gmail.com>
2022-10-05 20:05:50 -0600

    Added VTK Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  View,
+  GeometryRepresentation,
+  PolyData,
+  CellData,
+  DataArray,
+} from "react-vtk-js";
+import * as React from "react";
+
+import data from "./data.json";
 
 function App() {
+  const [show, setShow] = React.useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setShow((prev) => !prev)}>
+        {show ? "Hide" : "Show"}
+      </button>
+      <Timer />
+
+      {show ? (
+        <div style={{ width: "100%", height: "100vh" }}>
+          <VTK />
+        </div>
+      ) : null}
     </div>
   );
 }
+
+const Timer = React.memo(() => {
+  const [time, setTime] = React.useState(1);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => setTime((prev) => prev + 1), 1);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <span>{time}</span>;
+});
+
+const VTK = React.memo(() => {
+  return (
+    <View>
+      <GeometryRepresentation>
+        <PolyData points={data.points.flat()} polys={data.polys.flat()}>
+          {Object.keys(data.cellData).map((name) => {
+            return (
+              <CellData key={name}>
+                <DataArray name={name} values={data.cellData[name]} />
+              </CellData>
+            );
+          })}
+        </PolyData>
+      </GeometryRepresentation>
+    </View>
+  );
+});
 
 export default App;
